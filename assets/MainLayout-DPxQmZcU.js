cde446ebd466eb259c84222c83f1a7214123bb3d
Viega03-KH <ahmadov2000000@gmail.com>
2025-10-14 13:37:16 +0500

    build new pach

---
+++ assets/MainLayout-DPxQmZcU.js
@@ -0,0 +1,76 @@
+import{u as H,j as I,r as h,b as _,g as z,h as t,o as c,e as n,w as u,d as s,t as b,k as V,l as B,m as D,a as v,n as L,c as f,p as U,q as A,s as x,v as k,x as M,y as S,z as O,T}from"./index-BpB4yho-.js";import{c as i}from"./createLucideIcon-DVJbfQUB.js";import{_ as F}from"./_plugin-vue_export-helper-DlAUqK2U.js";/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const N=i("bell",[["path",{d:"M10.268 21a2 2 0 0 0 3.464 0",key:"vwvbt9"}],["path",{d:"M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326",key:"11g9vi"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const R=i("book-down",[["path",{d:"M12 13V7",key:"h0r20n"}],["path",{d:"M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20",key:"k3hazp"}],["path",{d:"m9 10 3 3 3-3",key:"zt5b4y"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const P=i("book-up",[["path",{d:"M12 13V7",key:"h0r20n"}],["path",{d:"M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20",key:"k3hazp"}],["path",{d:"m9 10 3-3 3 3",key:"11gsxs"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Z=i("calendar-clock",[["path",{d:"M16 14v2.2l1.6 1",key:"fo4ql5"}],["path",{d:"M16 2v4",key:"4m81vk"}],["path",{d:"M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5",key:"1osxxc"}],["path",{d:"M3 10h5",key:"r794hk"}],["path",{d:"M8 2v4",key:"1cmpym"}],["circle",{cx:"16",cy:"16",r:"6",key:"qoo3c4"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const E=i("chevron-down",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const G=i("chevron-right",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const $=i("file-check-2",[["path",{d:"M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4",key:"1pf5j1"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"m3 15 2 2 4-4",key:"1lhrkk"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const q=i("file-minus-2",[["path",{d:"M4 22h14a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v4",key:"1pf5j1"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M3 15h6",key:"4e2qda"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=i("file-search",[["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M4.268 21a2 2 0 0 0 1.727 1H18a2 2 0 0 0 2-2V7l-5-5H6a2 2 0 0 0-2 2v3",key:"ms7g94"}],["path",{d:"m9 18-1.5-1.5",key:"1j6qii"}],["circle",{cx:"5",cy:"14",r:"3",key:"ufru5t"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const J=i("file-text",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}],["path",{d:"M10 9H8",key:"b1mrlr"}],["path",{d:"M16 13H8",key:"t4e002"}],["path",{d:"M16 17H8",key:"z1uh3a"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const K=i("folder",[["path",{d:"M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z",key:"1kt360"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Q=i("house",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const W=i("languages",[["path",{d:"m5 8 6 6",key:"1wu5hv"}],["path",{d:"m4 14 6-6 2-3",key:"1k1g8d"}],["path",{d:"M2 5h12",key:"or177f"}],["path",{d:"M7 2h1",key:"1t2jsx"}],["path",{d:"m22 22-5-10-5 10",key:"don7ne"}],["path",{d:"M14 18h6",key:"1m8k6r"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const X=i("menu",[["path",{d:"M4 12h16",key:"1lakjw"}],["path",{d:"M4 18h16",key:"19g7jn"}],["path",{d:"M4 6h16",key:"1o0s65"}]]);/**
+ * @license lucide-vue-next v0.542.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const Y=i("moon",[["path",{d:"M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401",key:"kfwtm"}]]),ee="/assets/user-DCi4H9jc.png",te={key:0,class:"relative"},oe={class:"mr-3 overflow-hidden rounded-full h-11 w-11"},ne={class:"block mr-1 font-medium text-theme-sm"},ae={__name:"UserInfoDock",setup(l){const e=H();return I(async()=>{e.token&&!e.user&&(await e.fetchUser(),console.log("User maʼlumotlari:",e.user))}),(o,a)=>{const r=h("n-image"),d=h("n-button");return t(e).user?(c(),_("div",te,[n(d,{quaternary:"",class:"flex items-center h-auto w-auto p-1"},{default:u(()=>[s("span",oe,[n(r,{lazy:"","preview-disabled":"",src:t(ee)},null,8,["src"])]),s("span",ne,b(t(e).user.firstname)+" "+b(t(e).user.lastname),1),n(t(E),{size:"20","stroke-width":"2"})]),_:1})])):z("",!0)}}},se={class:"flex items-center gap-2 cursor-pointer"},le={__name:"UserDock",setup(l){const e=V(),o=H(),a=[{label:"Profil",key:"profile"},{label:"Sozlamalar",key:"settings"},{label:"Chiqish",key:"logout"}],r=async p=>{p==="logout"?await d():p==="profile"?e.push({name:"UserProfile"}):p==="settings"&&e.push({name:"Settings"})},d=async()=>{try{await o.logoutUser(),localStorage.removeItem("token"),e.push({name:"login"})}catch(p){console.error("Chiqish vaqtida xatolik:",p)}};return(p,w)=>{const g=h("n-dropdown");return c(),_("div",se,[n(g,{class:"w-64",trigger:"click",options:a,onSelect:r},{default:u(()=>[n(ae)]),_:1})])}}},re=F(le,[["__scopeId","data-v-dc6ba00b"]]),ce=B("lang",()=>{const{locale:l}=D(),e=v(localStorage.getItem("lang_id")||"uz");return L(e,a=>{l.value=a,localStorage.setItem("lang_id",a)},{immediate:!0}),{lang:e,setLang:a=>{e.value=a}}}),ie={__name:"LangDock",setup(l){const e=["uz","ru","en"],o=ce(),a=e.map(d=>({key:d,label:d.toUpperCase()}));function r(d){o.setLang(d)}return(d,p)=>{const w=h("n-button"),g=h("n-dropdown");return c(),f(g,{trigger:"click",options:t(a),onSelect:r},{default:u(()=>[n(w,{class:"relative flex items-center justify-center text-gray-500 transition-colors bg-white border border-gray-200 rounded-full h-11 w-11"},{default:u(()=>[n(t(W))]),_:1})]),_:1},8,["options"])}}},de={__name:"ThameDock",setup(l){return(e,o)=>{const a=h("n-button");return c(),f(a,{class:"relative flex items-center justify-center text-gray-500 transition-colors bg-white border border-gray-200 rounded-full h-11 w-11"},{default:u(()=>[n(t(Y))]),_:1})}}},ue={class:"relative"},he={__name:"BellDock",setup(l){return(e,o)=>{const a=h("n-button");return c(),_("div",ue,[n(a,{type:"default",class:"relative flex items-center justify-center text-gray-500 transition-colors bg-white border border-gray-200 rounded-full h-11 w-11"},{default:u(()=>[n(t(N)),o[0]||(o[0]=s("span",{class:"flex absolute right-0 top-0.5 z-1 h-2 w-2 rounded-full bg-orange-400"},[s("span",{class:"absolute inline-flex w-full h-full bg-orange-400 rounded-full opacity-75 -z-1 animate-ping"})],-1))]),_:1})])}}},pe={class:"flex w-full items-center justify-between h-16 px-4 bg-white border-b"},_e={class:"flex items-center"},me={class:"hidden items-center justify-between w-full px-5 gap-2 py-4 xl:flex xl:justify-end xl:px-0"},fe={__name:"NavbarDock",emits:["toggle-sidebar"],setup(l,{emit:e}){const o=e;return(a,r)=>{const d=h("n-button");return c(),_("div",pe,[s("div",_e,[n(d,{quaternary:"",circle:"",onClick:r[0]||(r[0]=p=>o("toggle-sidebar"))},{default:u(()=>[n(t(X),{"stroke-width":2})]),_:1})]),s("div",me,[n(he),n(de,{"toggle-theme":a.toggleTheme,"is-dark":a.isDark},null,8,["toggle-theme","is-dark"]),n(ie),n(re)])])}}},be={class:"ml-4 text-md font-bold"},y={__name:"SidebarLink",props:{icon:{type:[Object,Function],required:!1},label:String,to:String},setup(l){const e=l,o=U(),a=A(()=>o.path===e.to);return(r,d)=>{const p=h("router-link");return c(),f(p,{to:l.to,class:k(["flex items-center w-full h-12 px-4 mt-2 rounded hover:text-gray-100 hover:bg-gray-500/50 transition duration-300",a.value?"text-gray-100 bg-gray-500/50":""])},{default:u(()=>[(c(),f(x(l.icon),{"stroke-width":2,class:"w-5 h-5"})),s("span",be,b(l.label),1)]),_:1},8,["to","class"])}}},ke={class:"flex items-center"},ge={class:"ml-4 text-md font-bold"},ye={class:"ml-8 origin-top"},ve={__name:"SidebarItem",props:{label:String,icon:{type:[Object,Function],required:!1}},setup(l){const e=v(!1);function o(){e.value=!e.value}return(a,r)=>(c(),_("li",null,[s("div",{class:"flex items-center w-full h-12 px-4 mt-2 rounded hover:text-gray-100 hover:bg-gray-500/50 justify-between cursor-pointer select-none transition duration-300",onClick:o},[s("div",ke,[(c(),f(x(l.icon),{"stroke-width":2,class:"w-5 h-5"})),s("span",ge,b(l.label),1)]),M(n(t(G),{class:k(["w-4 h-4 text-gray-400 transition-transform duration-300",{"rotate-90":e.value}])},null,8,["class"]),[[S,a.$slots.default]])]),n(T,{name:"slide"},{default:u(()=>[M(s("ul",ye,[O(a.$slots,"default",{},void 0,!0)],512),[[S,e.value&&a.$slots.default]])]),_:3})]))}},j=F(ve,[["__scopeId","data-v-8b8104a1"]]),xe={class:"ml-4 text-md font-bold"},m={__name:"SidebarSubItem",props:{icon:{type:[Object,Function],required:!1},label:String,to:String,active:Boolean},setup(l){return(e,o)=>{const a=h("router-link");return c(),f(a,{to:l.to,class:"flex items-center w-full h-12 px-4 mt-2 rounded hover:text-gray-100 hover:bg-gray-500/50 cursor-pointer transition duration-300"},{default:u(()=>[(c(),f(x(l.icon),{"stroke-width":2,class:"w-5 h-5"})),s("span",xe,b(l.label),1)]),_:1},8,["to"])}}},we={class:"flex flex-col items-center justify-center px-4 py-8 border-b border-gray-700"},Me={class:"text-lg text-white font-bold text-center"},Se={class:"flex flex-col items-center w-72 h-full overflow-hidden text-gray-400"},$e={class:"w-full px-2"},qe={class:"flex flex-col items-center w-full mt-3"},Ce={class:"w-full"},je={__name:"SidebarDock",props:{isOpen:Boolean},emits:["close-sidebar"],setup(l,{emit:e}){const{t:o}=D();return(a,r)=>(c(),_("aside",{class:k(["fixed top-0 left-0 h-full w-72 bg-gray-900 text-gray-300 transform transition-transform duration-300 z-20",l.isOpen?"translate-x-0":"-translate-x-72"])},[s("div",we,[r[0]||(r[0]=s("svg",{class:"w-20 h-20 fill-current text-white mb-2"},[s("circle",{cx:"30",cy:"30",r:"30"})],-1)),s("span",Me,b(a.$t("sidebar.title")),1)]),s("div",Se,[s("div",$e,[s("div",qe,[s("ul",Ce,[n(y,{icon:t(Q),label:t(o)("sidebar.home"),to:"/"},null,8,["icon","label"]),n(y,{icon:t(Z),label:t(o)("sidebar.calendar"),to:"/dock-calendar"},null,8,["icon","label"]),n(j,{label:t(o)("sidebar.sent"),icon:t(P)},{default:u(()=>[n(m,{icon:t(J),label:t(o)("sidebar.sent_all"),to:"/dock-filetext"},null,8,["icon","label"]),n(m,{icon:t(C),label:t(o)("sidebar.sent_in_progress"),to:"/dock-FileSearch"},null,8,["icon","label"]),n(m,{icon:t($),label:t(o)("sidebar.sent_completed"),to:"/dock-FileCheck"},null,8,["icon","label"]),n(m,{icon:t(q),label:t(o)("sidebar.sent_uncompleted"),to:"/dock-FileMinus"},null,8,["icon","label"])]),_:1},8,["label","icon"]),n(j,{label:t(o)("sidebar.received"),icon:t(R)},{default:u(()=>[n(m,{icon:t(C),label:t(o)("sidebar.received_in_progress"),to:"/dock-in-execution"},null,8,["icon","label"]),n(m,{icon:t($),label:t(o)("sidebar.received_completed"),to:"/"},null,8,["icon","label"]),n(m,{icon:t(q),label:t(o)("sidebar.received_uncompleted"),to:"/"},null,8,["icon","label"])]),_:1},8,["label","icon"]),n(y,{icon:t(K),label:t(o)("sidebar.my_documents"),to:"/dock-Folder"},null,8,["icon","label"])])])])]),r[1]||(r[1]=s("div",{class:"p-4 border-t border-gray-700"},null,-1))],2))}},He={class:"flex h-screen bg-gray-100"},Ie={__name:"MainLayout",setup(l){const e=v(!0);function o(){e.value=!e.value}return(a,r)=>{const d=h("router-view");return c(),_("div",He,[n(je,{"is-open":e.value,onCloseSidebar:o},null,8,["is-open"]),e.value?(c(),_("div",{key:0,class:"fixed inset-0 bg-black bg-opacity-50 z-10 md:hidden",onClick:o})):z("",!0),s("main",{class:k(["flex-1 transition-all duration-300 w-full overflow-y-auto p-1",e.value?"md:ml-72":"ml-0"])},[n(fe,{onToggleSidebar:o}),s("div",null,[n(d)])],2)])}}};export{Ie as default};
